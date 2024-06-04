22e4e1c6377232400760a5381971b56aaa7420a4
arturo <arturo.veliz@ucb.edu.bo>
2024-06-04 14:25:02 -0400

    sign out añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mi-tienda';
+  router: Router=inject(Router);
+  signOut(): void {
+    // Eliminar el token del almacenamiento local (localStorage)
+    localStorage.removeItem('token');
+    
+    // Redirigir al usuario a la página de inicio de sesión o inicio
+    this.router.navigate(['/inicio']);
+  }
 }
